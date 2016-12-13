85281af00cd81f63e4110550a0bb92d31a3aec39
Matt Henderson <matt_henderson@sil.org>
2016-12-13 10:30:42 -0500

    Update PHPUnit tests for Yii models.

--- src/tests/unit/models/UserTest.php
+++ src/tests/unit/models/UserTest.php
@@ -9,10 +9,21 @@ class UserTest extends TestCase
     public function testChangingAnExistingUuid()
     {
         // Arrange:
+        $uniqId = uniqid();
         $user = new User();
+        $user->attributes = [
+            'email' => $uniqId . '@example.com',
+            'employee_id' => $uniqId,
+            'first_name' => 'Test ' . $uniqId,
+            'last_name' => 'User',
+            'username' => 'user' . $uniqId,
+        ];
         
         // Pre-assert:
-        $this->assertTrue($user->save());
+        $this->assertTrue($user->save(), sprintf(
+            'Failed to create User for test: %s',
+            print_r($user->getErrors(), true)
+        ));
         $this->assertTrue($user->refresh());
         
         // Act:
@@ -31,25 +42,5 @@ class UserTest extends TestCase
         
         // Assert:
         $this->assertNotEmpty($uuid);
-    }
-    
-    public function testValidationRules()
-    {
-        // Arrange:
-        $testCases = [
-        ];
-        foreach ($testCases as $testName => $testData) {
-
-            // Act:
-            $user = new User();
-            $user->attributes = $testData['attributes'];
-
-            // Assert:
-            $this->assertSame($testData['expected'], $user->validate(), sprintf(
-                'Incorrectly %s a User with %s.',
-                ($testData['expected'] ? 'rejected' : 'allowed'),
-                $testName
-            ));
-        }
     }
 }
