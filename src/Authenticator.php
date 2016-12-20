5275699355deb1f4e0cb9219c9cccb3f44d44ebb
Matt Henderson <matt_henderson@sil.org>
2016-12-20 17:15:24 -0500

    Add todo about implementing Captcha.

--- src/Authenticator.php
+++ src/Authenticator.php
@@ -41,6 +41,9 @@ class Authenticator
         
         /* @todo Make sure the CSRF has been validated. */
         
+        /* @todo If enough failed logins have occurred that we require a
+         *       Captcha, make sure the Captcha is correct.  */
+        
         $user = User::findByUsername($username);
         if ($user === null) {
 
