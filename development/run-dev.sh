fbcd7f1d3fcbf57b57c124c338d84d183e6addf0
Matt Henderson <matt_henderson@sil.org>
2017-01-09 16:26:00 -0500

    Add more xdebug support.

--- development/run-dev.sh
+++ development/run-dev.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-ln -s /silauth /data/vendor/simplesamlphp/simplesamlphp/modules/silauth
+rm -rf /data/vendor/simplesamlphp/simplesamlphp/modules/silauth
+ln -s /silauth /data/vendor/simplesamlphp/simplesamlphp/modules/
 
 ln -s /silauth/development/ssp/authsources.php /data/vendor/simplesamlphp/simplesamlphp/config/authsources.php
 ln -s /silauth/development/ssp/saml20-idp-hosted.php /data/vendor/simplesamlphp/simplesamlphp/metadata/saml20-idp-hosted.php
+mkdir -p /data/vendor/simplesamlphp/simplesamlphp/cert/
 ln -s /silauth/development/ssp/saml.crt /data/vendor/simplesamlphp/simplesamlphp/cert/saml.crt
 ln -s /silauth/development/ssp/saml.pem /data/vendor/simplesamlphp/simplesamlphp/cert/saml.pem
+
+apt-get install php-xdebug -y
 
 /data/run.sh
