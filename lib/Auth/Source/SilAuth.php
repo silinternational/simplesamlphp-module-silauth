09b0fd34912bf3cb199e2bb985b857de30851410
Matt Henderson <matt_henderson@sil.org>
2017-01-12 16:17:25 -0500

    Override UserPassBase->authenticate(...) to force use of SilAuth's loginuserpass.php.

--- lib/Auth/Source/SilAuth.php
+++ lib/Auth/Source/SilAuth.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 'On');
+
 use Sil\SilAuth\auth\Authenticator;
 use Sil\SilAuth\auth\AuthError;
 
@@ -28,6 +30,39 @@ class sspmod_silauth_Auth_Source_SilAuth extends sspmod_core_Auth_UserPassBase
         
         require_once __DIR__ . '/../../../src/bootstrap-yii2.php';
     }
+
+	/**
+	 * Initialize login.
+	 *
+	 * This function saves the information about the login, and redirects to a
+	 * login page.
+	 *
+	 * @param array &$state  Information about the current authentication.
+	 */
+	public function authenticate(&$state) {
+		assert('is_array($state)');
+
+		/*
+		 * Save the identifier of this authentication source, so that we can
+		 * retrieve it later. This allows us to call the login()-function on
+		 * the current object.
+		 */
+		$state[self::AUTHID] = $this->authId;
+
+		/* Save the $state-array, so that we can restore it after a redirect. */
+		$id = SimpleSAML_Auth_State::saveState($state, self::STAGEID);
+
+		/*
+		 * Redirect to the login form. We include the identifier of the saved
+		 * state array as a parameter to the login form.
+		 */
+		$url = SimpleSAML_Module::getModuleURL('silauth/loginuserpass.php');
+		$params = array('AuthState' => $id);
+		\SimpleSAML\Utils\HTTP::redirectTrustedURL($url, $params);
+
+		/* The previous function never returns, so this code is never executed. */
+		assert('FALSE');
+	}
     
     protected function login($username, $password)
     {
