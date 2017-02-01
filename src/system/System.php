ae5ffcbc5d57f8583872711295c944179b7d9bdc
Matt Henderson <matt_henderson@sil.org>
2017-02-01 16:49:11 -0500

    Require SSP's baseurlpath to be an absolute URL.

--- src/system/System.php
+++ src/system/System.php
@@ -44,6 +44,22 @@ class System
         }
     }
     
+    protected function isRequiredConfigPresent()
+    {
+        $globalConfig = \SimpleSAML_Configuration::getInstance();
+        
+        /*
+         * NOTE: We require that SSP's baseurlpath configuration is set (and
+         *       matches the corresponding RegExp) in order to prevent a
+         *       security hole in \SimpleSAML\Utils\HTTP::getBaseURL() where the
+         *       HTTP_HOST value (provided by the user's request) is used to
+         *       build a trusted URL (see SimpleSaml\Module::authenticate()).
+         */
+        $baseURL = $globalConfig->getString('baseurlpath', '');
+        $avoidsSecurityHole = (preg_match('#^https?://.*/$#D', $baseURL) === 1);
+        return $avoidsSecurityHole;
+    }
+    
     /**
      * Check the status of the system, and throw an exception (that is safe to
      * show to the public) if any serious error conditions are found. Log any
@@ -53,6 +69,10 @@ class System
      */
     public function reportStatus()
     {
+        if ( ! $this->isRequiredConfigPresent()) {
+            $this->reportError('Config problem', 1485984755);
+        }
+        
         if ( ! $this->isDatabaseOkay()) {
             $this->reportError('Database problem', 1485284407);
         }
