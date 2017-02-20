6710490c9545962c57c862b6bbe1f14d99e5297e
Matt Henderson <matt_henderson@sil.org>
2017-02-20 16:52:58 -0500

    Add test for countRecentFailedLoginsFor().

---
+++ src/tests/unit/models/FailedLoginUsernameTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Sil\SilAuth\tests\unit\models;
+
+use Sil\SilAuth\models\FailedLoginUsername;
+use Sil\SilAuth\time\UtcTime;
+use PHPUnit\Framework\TestCase;
+
+class FailedLoginUsernameTest extends TestCase
+{
+    protected function setDbFixture($recordsData)
+    {
+        FailedLoginUsername::deleteAll();
+        foreach ($recordsData as $recordData) {
+            $model = new FailedLoginUsername($recordData);
+            $this->assertTrue($model->insert(false));
+        }
+    }
+    
+    public function testCountRecentFailedLoginsFor()
+    {
+        // Arrange:
+        $username = 'john_smith';
+        $fixtures = [[
+            'username' => $username,
+            'occurred_at_utc' => UtcTime::format('-61 minutes'),
+        ], [
+            'username' => $username,
+            'occurred_at_utc' => UtcTime::format('-59 minutes'),
+        ], [
+            'username' => $username,
+            'occurred_at_utc' => UtcTime::format(),
+        ]];
+        $this->setDbFixture($fixtures);
+        
+        
+        // Pre-assert:
+        $this->assertCount(
+            count($fixtures),
+            FailedLoginUsername::getFailedLoginsFor($username)
+        );
+
+        // Act:
+        $result = FailedLoginUsername::countRecentFailedLoginsFor($username);
+
+        // Assert:
+        $this->assertEquals(2, $result);
+    }
+}
