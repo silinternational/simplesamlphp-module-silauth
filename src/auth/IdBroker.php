1d4357a8cfa1c623e055e6056e0918961f2b5af2
Matt Henderson <matt_henderson@sil.org>
2017-02-20 16:45:53 -0500

    Add some basic functions to the IdBroker class.

--- src/auth/IdBroker.php
+++ src/auth/IdBroker.php
@@ -1,7 +1,20 @@
 <?php
 namespace Sil\SilAuth\auth;
 
+use Psr\Log\LoggerInterface;
+
 class IdBroker
 {
+    /** @var LoggerInterface */
+    protected $logger;
     
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    
+    public function getUserAttributesFor($username)
+    {
+        throw new \Exception('Not yet implemented.');
+    }
 }
