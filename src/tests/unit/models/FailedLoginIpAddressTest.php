ed49827f90376965f81147f857ff9b978ab3cfbc
Matt Henderson <matt_henderson@sil.org>
2017-02-21 16:35:55 -0500

    Begin test file for FailedLoginIpAddress.

---
+++ src/tests/unit/models/FailedLoginIpAddressTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Sil\SilAuth\tests\unit\models;
+
+use Sil\SilAuth\log\Psr3ConsoleLogger;
+use Sil\SilAuth\models\FailedLoginIpAddress;
+use Sil\SilAuth\time\UtcTime;
+use PHPUnit\Framework\TestCase;
+
+class FailedLoginIpAddressTest extends TestCase
+{
+}
