4f7e3dbb5da592f7b866b865ba2467290fab0e55
Phillip Shipley <phillip.shipley@gmail.com>
2017-10-24 15:20:39 -0400

    final prep?

--- src/auth/Authenticator.php
+++ src/auth/Authenticator.php
@@ -20,19 +20,19 @@ class Authenticator
     const REQUIRE_CAPTCHA_AFTER_NTH_FAILED_LOGIN = 10;
     const BLOCK_AFTER_NTH_FAILED_LOGIN = 50;
     const MAX_SECONDS_TO_BLOCK = 3600; // 3600 seconds = 1 hour
-
+    
     /** @var AuthError|null */
     private $authError = null;
-
+    
     /** @var LoggerInterface */
     protected $logger;
-
+    
     private $userAttributes = null;
-
+    
     /**
      * Attempt to authenticate using the given username and password. Check
      * isAuthenticated() to see whether authentication was successful.
-     *
+     * 
      * @param string $username The username to check.
      * @param string $password The password to check.
      * @param Request $request An object representing the HTTP request.
@@ -41,36 +41,36 @@ class Authenticator
      * @param LoggerInterface $logger A PSR-3 compliant logger.
      */
     public function __construct(
-        $username,
-        $password,
-        Request $request,
-        Captcha $captcha,
-        IdBroker $idBroker,
-        LoggerInterface $logger
+            $username,
+            $password,
+            Request $request,
+            Captcha $captcha,
+            IdBroker $idBroker,
+            LoggerInterface $logger
     ) {
         $this->logger = $logger;
-
+        
         /** @todo Check CSRF here, too, if feasible. */
-
+        
         if (empty($username)) {
             $this->setErrorUsernameRequired();
             return;
         }
-
+        
         if (empty($password)) {
             $this->setErrorPasswordRequired();
             return;
         }
-
+        
         $ipAddresses = $request->getUntrustedIpAddresses();
-
+        
         if ($this->isBlockedByRateLimit($username, $ipAddresses)) {
             $this->setErrorBlockedByRateLimit(
                 $this->getWaitTimeUntilUnblocked($username, $ipAddresses)
             );
             return;
         }
-
+        
         if (self::isCaptchaRequired($username, $ipAddresses)) {
             $logger->warning('Captcha required for {username} (IP: {ipAddresses}).', [
                 'username' => var_export($username, true),
@@ -85,7 +85,7 @@ class Authenticator
                 return;
             }
         }
-
+        
         try {
             $authenticatedUser = $idBroker->getAuthenticatedUser(
                 $username,
@@ -100,10 +100,10 @@ class Authenticator
             $this->setErrorGenericTryLater();
             return;
         }
-
+        
         if ($authenticatedUser === null) {
             $this->recordFailedLoginBy($username, $ipAddresses);
-
+            
             if ($this->isBlockedByRateLimit($username, $ipAddresses)) {
                 $this->setErrorBlockedByRateLimit(
                     $this->getWaitTimeUntilUnblocked($username, $ipAddresses)
@@ -113,14 +113,14 @@ class Authenticator
             }
             return;
         }
-
+        
         // NOTE: If we reach this point, the user successfully authenticated.
-
+        
         $this->resetFailedLoginsBy($username, $ipAddresses);
-
+        
         $this->setUserAttributes($authenticatedUser);
     }
-
+    
     /**
      * Calculate how many seconds of delay should be required for the given
      * number of recent failed login attempts.
@@ -134,27 +134,27 @@ class Authenticator
         if ( ! self::isEnoughFailedLoginsToBlock($numRecentFailures)) {
             return 0;
         }
-
+        
         $limit = self::BLOCK_AFTER_NTH_FAILED_LOGIN;
         $numFailuresPastLimit = $numRecentFailures - $limit;
         $numberToUse = max($numFailuresPastLimit, 3);
-
+        
         return min(
             $numberToUse * $numberToUse,
             self::MAX_SECONDS_TO_BLOCK
         );
     }
-
+    
     /**
      * Get the error information (if any).
-     *
+     * 
      * @return AuthError|null
      */
     public function getAuthError()
     {
         return $this->authError;
     }
-
+    
     /**
      * Get the number of seconds to continue blocking, based on the given number
      * of recent failures and the given date/time of the most recent failed
@@ -175,21 +175,21 @@ class Authenticator
         if ($mostRecentFailureAt === null) {
             return 0;
         }
-
+        
         $totalSecondsToBlock = self::calculateSecondsToDelay(
             $numRecentFailures
         );
-
+        
         $secondsSinceLastFailure = UtcTime::getSecondsSinceDateTime(
             $mostRecentFailureAt
         );
-
+        
         return UtcTime::getRemainingSeconds(
             $totalSecondsToBlock,
             $secondsSinceLastFailure
         );
     }
-
+    
     /**
      * Get the attributes about the authenticated user.
      *
@@ -210,10 +210,10 @@ class Authenticator
                 1482270373
             );
         }
-
+        
         return $this->userAttributes;
     }
-
+    
     /**
      * Get a (user friendly) wait time representing how long the user must wait
      * until they will no longer be blocked by a rate limit (regardless of
@@ -233,69 +233,69 @@ class Authenticator
         $durationsInSeconds = [
             FailedLoginUsername::getSecondsUntilUnblocked($username),
         ];
-
+        
         foreach ($ipAddresses as $ipAddress) {
             $durationsInSeconds[] = FailedLoginIpAddress::getSecondsUntilUnblocked($ipAddress);
         }
-
+        
         return WaitTime::getLongestWaitTime($durationsInSeconds);
     }
-
+    
     protected function hasError()
     {
         return ($this->authError !== null);
     }
-
+    
     /**
      * Check whether authentication was successful. If not, call
      * getErrorMessage() and/or getErrorCode() to find out why not.
-     *
+     * 
      * @return bool
      */
     public function isAuthenticated()
     {
         return ( ! $this->hasError());
     }
-
+    
     protected function isBlockedByRateLimit($username, array $ipAddresses)
     {
         return FailedLoginUsername::isRateLimitBlocking($username) ||
-            FailedLoginIpAddress::isRateLimitBlockingAnyOfThese($ipAddresses);
+               FailedLoginIpAddress::isRateLimitBlockingAnyOfThese($ipAddresses);
     }
-
+    
     public static function isCaptchaRequired($username, array $ipAddresses)
     {
         return FailedLoginUsername::isCaptchaRequiredFor($username) ||
-            FailedLoginIpAddress::isCaptchaRequiredForAnyOfThese($ipAddresses);
+               FailedLoginIpAddress::isCaptchaRequiredForAnyOfThese($ipAddresses);
     }
-
+    
     public static function isEnoughFailedLoginsToBlock($numFailedLogins)
     {
         return ($numFailedLogins >= self::BLOCK_AFTER_NTH_FAILED_LOGIN);
     }
-
+    
     public static function isEnoughFailedLoginsToRequireCaptcha($numFailedLogins)
     {
         return ($numFailedLogins >= self::REQUIRE_CAPTCHA_AFTER_NTH_FAILED_LOGIN);
     }
-
+    
     protected function recordFailedLoginBy($username, array $ipAddresses)
     {
         FailedLoginUsername::recordFailedLoginBy($username, $this->logger);
         FailedLoginIpAddress::recordFailedLoginBy($ipAddresses, $this->logger);
     }
-
+    
     protected function resetFailedLoginsBy($username, array $ipAddresses)
     {
         FailedLoginUsername::resetFailedLoginsBy($username);
         FailedLoginIpAddress::resetFailedLoginsBy($ipAddresses);
     }
-
+    
     protected function setError($code, $messageParams = [])
     {
         $this->authError = new AuthError($code, $messageParams);
     }
-
+    
     /**
      * @param WaitTime $waitTime
      */
@@ -303,7 +303,7 @@ class Authenticator
     {
         $unit = $waitTime->getUnit();
         $number = $waitTime->getFriendlyNumber();
-
+        
         if ($unit === WaitTime::UNIT_SECOND) {
             $errorCode = AuthError::CODE_RATE_LIMIT_SECONDS;
         } else { // = minute
@@ -313,35 +313,35 @@ class Authenticator
                 $errorCode = AuthError::CODE_RATE_LIMIT_MINUTES;
             }
         }
-
+        
         $this->setError($errorCode, ['{number}' => $number]);
     }
-
+    
     protected function setErrorGenericTryLater()
     {
         $this->setError(AuthError::CODE_GENERIC_TRY_LATER);
     }
-
+    
     protected function setErrorInvalidLogin()
     {
         $this->setError(AuthError::CODE_INVALID_LOGIN);
     }
-
+    
     protected function setErrorNeedToSetAcctPassword()
     {
         $this->setError(AuthError::CODE_NEED_TO_SET_ACCT_PASSWORD);
     }
-
+    
     protected function setErrorPasswordRequired()
     {
         $this->setError(AuthError::CODE_PASSWORD_REQUIRED);
     }
-
+    
     protected function setErrorUsernameRequired()
     {
         $this->setError(AuthError::CODE_USERNAME_REQUIRED);
     }
-
+    
     protected function setUserAttributes($attributes)
     {
         $this->userAttributes = $attributes;
